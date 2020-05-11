29963411ddaf09039b102462bfe96f76bdba6ff7
Emma E. Corbett <20eec4@lohani.cs.williams.edu>
2020-05-11 14:21:02 -0400

    weird error

--- Peer.java
+++ Peer.java
@@ -16,12 +16,16 @@ import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
   Running Questions:
   - Does the host need to be the same amongst all peers? 
 */
+/*
+  To Do:
+  - each peer should have vector of its connectiosn to other peers  
+*/
 
 public class Peer {
 
-    // global variables for stateful server
-    static HashMap<Integer, Integer> purchaselog;
-    static Connection c = null;
+    // global variables for stateful server   
+    //    public XmlRpcClient client; 
+    //public XmlRpcClientConfigImpl config;
 
     public Peer(int num, int port) {
 	// Create connection to the database
@@ -35,7 +39,7 @@ public class Peer {
 	    // Set up the webserver 
 	    WebServer server = new WebServer(port);
 	    xmlRpcServer = server.getXmlRpcServer();
-	    phm.addHandler("peer" + Integer.toString(num), Peer.class);
+	    phm.addHandler("peer" + num, Peer.class);
 	    xmlRpcServer.setHandlerMapping(phm);
 	    server.start();
 	    System.out.println("Started successfully.");
@@ -50,6 +54,8 @@ public class Peer {
 	// Connect to other peers
 	XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
 	XmlRpcClient client=null;
+	//config = new XmlRpcClientConfigImpl();
+	//client = null;
 	try {
 	    config.setServerURL(new URL("http://lohani.cs.williams.edu:" + port));
 	    // host may need to be passed through as parameter... TBD 
@@ -63,6 +69,47 @@ public class Peer {
 	return 1; 
     }
 
+    // Send string input directly- may want to make node name instead of port, TBD (could do a hash table with key- node name, value- port) 
+    public int receive(String peerName, String fileName) {
+	XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+	XmlRpcClient client = null;
+	try {
+	    config.setServerURL(new URL("http://lohani.cs.williams.edu:" + 9174));
+	    // host may need to be passed through as parameter... TBD
+	    // config.setServerURL(new URL("http://" + host + ":" + port));
+	    client = new XmlRpcClient();
+	    client.setConfig(config);
+	} catch (Exception e) {
+	    System.out.println("Problem connecting to server!");
+	    return 0;
+	}
+
+	try {
+	    Vector<String> params = new Vector<String>();
+	    params.addElement(fileName);
+	    //	    String[] input = new String[1];
+	    //input[0] = fileName; 
+	    System.out.println("Before execute: " + peerName);
+	    Object[] result = (Object[])client.execute(peerName+".sendFile", params.toArray());
+	    String output = (String) result[0];
+	    System.out.println(output); 	    
+	    return 1; 
+	} catch (Exception e) {
+	    System.out.println("Receive error: " + e);
+	}	
+	return 0;
+    }
+
+   
+    public String[] sendFile(String fileName) {	
+	System.out.println("In send");
+	// see if file exists
+	// open file
+	// read line by line, send contents
+	String[] result = new String[1];
+	result[0] = "Hello!";
+	return result; 
+    }
     
     // Set up server and interface to allow operations from stdin and clients 
     public static void main (String [] args) {
@@ -70,8 +117,12 @@ public class Peer {
 	    Peer peer1 = new Peer(1, 9173);
 	    Peer peer2 = new Peer(2, 9174);
 
-	    peer1.connect(9174);
-	    System.out.println("Successful connection of 1 to 2");
+	    //peer1.connect(9174);
+	    //System.out.println("Successful connection of 1 to 2");
+
+	    peer1.receive("peer2", "Hello!");
+	    System.out.println("Receive complete.");
+
 	    /*
 	    // Reads user commmands from standard in- reprompts if not valid command
 	    Scanner in = new Scanner(System.in);
