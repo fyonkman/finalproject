a0732dfc5a6e7b64f107cac5834e3ce3da473b7c
Emma E. Corbett <20eec4@lohani.cs.williams.edu>
2020-05-12 09:51:40 -0400

    cleaned up code

--- Peer.java
+++ Peer.java
@@ -18,20 +18,14 @@ import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 */
 /*
   To Do:
-  - each peer should have vector of its connectiosn to other peers  
+  - each peer should have vector of its connectiosn to other peers? Unless we close a connection after sending...   
 */
 
 public class Peer {
 
-    // global variables for stateful server   
-    //    public XmlRpcClient client; 
-    //public XmlRpcClientConfigImpl config;
 
     public Peer(int num, int port) {
-	// Create connection to the database
-	//Class.forName("org.sqlite.JDBC");
-	//c = DriverManager.getConnection("jdbc:sqlite:bookstore.db");
-	//c.setAutoCommit(false);
+	// Start 'server' aspect of the peer, named "peer#" where # is the num passed through
 	try {
 	    PropertyHandlerMapping phm = new PropertyHandlerMapping();
 	    XmlRpcServer xmlRpcServer;
@@ -48,8 +42,8 @@ public class Peer {
 	    System.out.println("Server exiting.");
 	}
     }
-
-    // connect this peer to peer specified by port number 
+    /*
+    // connect this peer to another peer specified by port number 
     public int connect(int port) {
 	// Connect to other peers
 	XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
@@ -68,12 +62,16 @@ public class Peer {
 	}      
 	return 1; 
     }
+    */
 
     // Send string input directly- may want to make node name instead of port, TBD (could do a hash table with key- node name, value- port) 
     public int receive(String peerName, String fileName) {
+	
+	// connect within the receive... TBD? - connecting peer1 to peer2 (hardcoded right now with 9174) 
 	XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
 	XmlRpcClient client = null;
 	try {
+	    // This is hardcoded to be peer 2
 	    config.setServerURL(new URL("http://lohani.cs.williams.edu:" + 9174));
 	    // host may need to be passed through as parameter... TBD
 	    // config.setServerURL(new URL("http://" + host + ":" + port));
@@ -84,12 +82,15 @@ public class Peer {
 	    return 0;
 	}
 
+	// calls execute on .sendFile
 	try {
 	    Vector<String> params = new Vector<String>();
 	    params.addElement(fileName);
 	    //	    String[] input = new String[1];
 	    //input[0] = fileName; 
 	    System.out.println("Before execute: " + peerName);
+	    
+	    // ********************* Error happens at this line of code 
 	    Object[] result = (Object[])client.execute(peerName+".sendFile", params.toArray());
 	    String output = (String) result[0];
 	    System.out.println(output); 	    
@@ -100,7 +101,7 @@ public class Peer {
 	return 0;
     }
 
-   
+    // test method to send "hello" between two peers... eventually will be contents of a file 
     public String[] sendFile(String fileName) {	
 	System.out.println("In send");
 	// see if file exists
@@ -111,7 +112,7 @@ public class Peer {
 	return result; 
     }
     
-    // Set up server and interface to allow operations from stdin and clients 
+    // Create peers, connect them, send messages.... 
     public static void main (String [] args) {
 	try {
 	    Peer peer1 = new Peer(1, 9173);
